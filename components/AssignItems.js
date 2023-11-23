df7a3043e8dea0db9a6cd672fe6b6d64f8c7f53f
Theo Breaux <theobreaux@gmail.com>
2023-11-23 12:48:20 -0800

    start integration of drag and drop

--- components/AssignItems.js
+++ components/AssignItems.js
@@ -1,14 +1,40 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Logo from "./Logo";
+import DinnerItemList from "./ui/DinnerItemList";
+import DinnerItem from "./ui/DinnerItem";
+
+const dinnerItems = [
+  { id: Date.now(), count: 1, name: "Chocolate Cake", price: 25.95 },
+  { id: Date.now(), count: 2, name: "Chicken Tacos", price: 5.95 },
+  { id: Date.now(), count: 2, name: "Casamigos Blanco", price: 32.0 },
+  { id: Date.now(), count: 1, name: "Mule", price: 20.0 },
+];
 
 const AssignItems = () => {
   return (
-    <View>
-      <Text>AssignItems</Text>
-    </View>
+    <>
+      <Logo />
+      <View style={styles.container}>
+        <DinnerItemList>
+          {dinnerItems.map((item) => {
+            <DinnerItem key={item.id} {...DinnerItem} />;
+          })}
+        </DinnerItemList>
+        <Text>ItemList</Text>
+      </View>
+    </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 0,
+    margin: 10,
+    borderWidth: 1,
+    borderBlockColor: "red",
+  },
+});
 
 export default AssignItems;
